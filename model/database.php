b693a9fe2e9dd59f45125e62d89b4221bf105824
revordj <70046093+revordj@users.noreply.github.com>
2021-03-15 20:33:23 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,11 +1,11 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    //$password = 'pa55word';
+    $dsn = 'mysql:host=xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=tok9nrasvgonhajh';
+    $username = 'y0uzwzfmp9elkru6';
+    $password = 'djyrnk2c0wij60f4';
 
     try{
-        $db = new PDO($dsn, $username);
-        //$db = new PDO($dsn, $username, $password);
+        //$db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
 
     }   catch(PDOException $e) {
         $error_message = 'Database Error: ';
