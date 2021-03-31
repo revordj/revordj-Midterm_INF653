e0a7fcabb3eb4158dda219521455a0e2ddf7b421
Joshua Revord <jarevord@mail.fhsu.edu>
2021-03-31 16:40:24 -0500

    r2

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
         $sortby= 'price';
     }
 
-    if($action = false || $action <> 'register'){
+    if($action = false){
         if(!$makeID && !$typesID && !$classesID){
             $inventory = get_inventory($sortby);
         }
@@ -75,7 +75,7 @@
     
         include('view/inventory.php');
     }
-    elseif($action == 'register'){
+    elseif($_GET[action] == 'register'){
         include('view/register.php');
     }
     else{
