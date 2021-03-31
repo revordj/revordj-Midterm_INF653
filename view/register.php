dc3f05d71e668eadcc7ba9e5715b84b4e18f477d
Joshua Revord <jarevord@mail.fhsu.edu>
2021-03-31 14:44:32 -0500

    testing local var

--- view/register.php
+++ view/register.php
@@ -1,8 +1,9 @@
 <?php include('view/header.php') ?>
 <?php 
 $testing = 'This is the test value';
+$actionlocal = $_GET['action'];
 ?>
 This is the register page
 
-The action variable is set to <?= $action ?>. The testing variable is set to: <?= $testing ?>
+The action variable is set to <?= $actionlocal ?>. The testing variable is set to: <?= $testing ?>
 <?php include('view/footer.php') ?>
