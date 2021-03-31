e39bcb31a0acffed5a30af0a0165833b8e8559a1
Joshua Revord <jarevord@mail.fhsu.edu>
2021-03-31 14:42:17 -0500

    t3

--- view/register.php
+++ view/register.php
@@ -1,6 +1,8 @@
 <?php include('view/header.php') ?>
-
+<?php 
+$testing = 'This is the test value';
+?>
 This is the register page
 
-The action variable is set to <?= $action ?>
+The action variable is set to <?= $action ?>. The testing variable is set to: <?= $testing ?>
 <?php include('view/footer.php') ?>
