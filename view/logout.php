222328530a9b4307d7f563b36f92125592bd5c25
Joshua Revord <jarevord@mail.fhsu.edu>
2021-03-31 20:36:30 -0500

    edited logout page

--- view/logout.php
+++ view/logout.php
@@ -1,3 +1,20 @@
 <?php include('view/header.php') ?>
-	This is the log off page
+	<section name='login_logout' id='login_logout'>
+		<p name='msg' id='msg'> Thank you for logging out, <?php echo($_SESSION['userid']) ?>! </p>
+		<p> <a href='index.php'> Click Here </a> to view our vehicle list. </a>
+</section>
+<?php 
+	$_SESSION = array(); //clear session data
+	session_destroy(); // clean session id
+
+	//delete the session cookie
+	$name = session_name();
+	$expire = strtotime('-1 year');
+	$params = session_get_cookie_params();
+	$path = $params['path'];
+	$domain = $params['domain'];
+	$secure = $params['secure'];
+	$httponly = $params['httponly'];
+	setcookie($name, '', $expire, $path, $domain, $secure, $httponly);
+
 <?php include('view/footer.php') ?>
